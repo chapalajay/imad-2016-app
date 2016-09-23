fff47d03e9ec5103bb1b0d083b04ab541c9b09c0
chapalajay <jbktrc@gmail.com>
2016-09-23 22:21:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-I',function(req,res)){
+    res.send('Article one is requested and will be served soon');
+});
+app.get('/article-II',function(req,res)){
+    res.send('Article two is requested and will be served soon');
+});
+app.get('/article-III',function(req,res)){
+    res.send('Article three is requested and will be served soon');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
