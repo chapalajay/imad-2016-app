41d40df33f265c8f016a1d372a35f29a1e96e7d9
Jay Chapala <jbktrc@gmail.com>
2016-10-10 20:22:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML ='Jay Chapala';
 var img=document.getElementById('madi');
+function moveRight(){
+    marginLeft += 10;
+    img.style.marginLeft=marginLeft +"px";
+}
 img.onClick =function(){
-    img.style.marginLeft = "100px";
+    var interval= setInterval(moveRight,100);
 };
