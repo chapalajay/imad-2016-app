66540d2132aec2e63708f2cdaec2e0eab526cd0d
Jay Chapala <jbktrc@gmail.com>
2016-10-10 18:56:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title : 'Article I || Jay Chapala',
     heading:'Article I',
     date:'Sep 5,2016',
